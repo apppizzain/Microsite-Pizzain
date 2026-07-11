3fbc085b7bd6e086900142ab297c41a81fca13fe
apppizzain <apppizzain@gmail.com>
2026-07-12 00:21:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const LINKS = {
   menu: "https://drive.google.com/file/d/1nEDGxb90NJI3LRCGiLe1j5v6masHLb4o/view",
   delivery: "https://wa.me/6282210069671?text=Assalamualaikum%20Pizzain%2C%20saya%20mau%20pesan%20delivery",
-  takeaway: "https://wa.me/6281285601788?text=Halo%20Pizza!n%2C%20saya%20mau%20pesan%20takeaway",
-  feedback: "https://wa.me/6282310946757?text=Halo%20Pizza!n%2C%20saya%20mau%20memberi%20kritik%20dan%20saran",
+  takeaway: "https://wa.me/6281285601788?text=Halo%20Pizzain%2C%20saya%20mau%20pesan%20takeaway",
+  feedback: "https://wa.me/6282310946757?text=Halo%20Pizzain%2C%20saya%20mau%20memberi%20kritik%20dan%20saran",
 };
 
 document.querySelectorAll("[data-link-key]").forEach((button) => {
